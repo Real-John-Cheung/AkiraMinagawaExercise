56d5ace77cb1a271cca872653ede976faf1b1225
JohnC <real.john.chueng@gmail.com>
2022-09-22 17:43:11 +0100

    init template

---
+++ triathlon/sketch.js
@@ -0,0 +1,22 @@
+let sketch = function (s) { 
+    let div = window.document.getElementsByClassName('sketchContainer')[0];
+
+    class Polygon{
+        constructor(side,center) {}
+    }
+
+    s.setup = function () {
+        s.createCanvas(Utils.elementWidth(div), (Utils.elementWidth(div)/16) * 10);
+        s.background(0);
+    }
+
+    s.draw = function () {
+        
+    }
+
+    s.windowResized = function () {
+        s.resizeCanvas(Utils.elementWidth(div), (Utils.elementWidth(div)/16) * 10);
+        s.background(0);
+    }
+    
+}
---
+++ utils.js
@@ -0,0 +1,15 @@
+class Utils {
+    // Calculate the Width in pixels of a Dom element
+    static elementWidth(element) {
+      return (
+        element.clientWidth
+      )
+    }
+  
+    // Calculate the Height in pixels of a Dom element
+    static elementHeight(element) {
+      return (
+        element.clientHeight
+      )
+    }
+  }
